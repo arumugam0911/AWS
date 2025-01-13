1ebeea6b4f6e60d5c4f8b888b413806021288ba8
arumugam <prabhakaranarumugam84@gmail.com>
2025-01-12 20:10:21 -0500

    data-block changes

---
+++ data-block/variables.tf
@@ -0,0 +1,15 @@
+variable "amiid"{
+  default = "vertex*"
+}
+variable "keypair" {
+  default = "ec2_keypair"
+}
+variable "instance_type" {
+  default = "t2.micro"
+}
+variable "linux_security_group"{
+  default = "linux_SG"
+}
+variable "instance_name"{
+  default = "terraform_ec2"
+}
