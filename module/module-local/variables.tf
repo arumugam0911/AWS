050c62ad3f4582d50b1a29e84cbe61eaf72f7869
arumugam <prabhakaranarumugam84@gmail.com>
2025-01-12 19:23:20 -0500

    folder rename

---
+++ module/module-local/variables.tf
@@ -0,0 +1,22 @@
+variable "amiid"{
+
+}
+variable "keypair" {
+
+}
+variable "instance_type" {
+
+}
+variable "module_security_group"{
+
+}
+variable "instance_name"{
+
+}
+variable "env" {
+
+}
+
+variable "cidr"{
+    
+}
